ecaf94944ab24d1bc8378ad1cd87b35c39044e45
erfaneshrati <erfaneshrati@gmail.com>
2022-11-15 15:58:21 -0800

    onnx export for retinaface detection model

--- inference/inference_detection.py
+++ inference/inference_detection.py
@@ -6,6 +6,21 @@ from facexlib.detection import init_detection_model
 from facexlib.visualization import visualize_detection
 
 
+def export_onnx(model, input):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    input = torch.Tensor([input]).to(device).permute(0,3,1,2) # Convert to NCHW
+    torch.onnx.export(model,  # model being run
+                      input,  # model input (or a tuple for multiple inputs)
+                      'restina_resnet50.onnx',  # where to save the model (can be a file or file-like object)
+                      export_params=True,  # store the trained parameter weights inside the model file
+                      opset_version=11,  # the ONNX version to export the model to
+                      do_constant_folding=True,  # whether to execute constant folding for optimization
+                      verbose=True,
+                      input_names=['image'],  # the model's input names
+                      output_names=['bboxes', 'classes', 'ldms']  # the model's output names
+                      )
+
+
 def main(args):
     # initialize model
     det_net = init_detection_model(args.model_name, half=args.half)
@@ -14,9 +29,9 @@ def main(args):
     with torch.no_grad():
         bboxes = det_net.detect_faces(img, 0.97)
         # x0, y0, x1, y1, confidence_score, five points (x, y)
-        print(bboxes)
+        print (bboxes)
         visualize_detection(img, bboxes, args.save_path)
-
+    export_onnx(det_net, img)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
